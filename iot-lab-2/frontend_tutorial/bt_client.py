328a1cb7b2b407ba6e25b1a9bf2ac0de64324a85
Vrush Patel <vrush2@illinois.edu>
2023-03-01 19:24:17 -0600

    app and frontend code

---
+++ iot-lab-2/frontend_tutorial/bt_client.py
@@ -0,0 +1,16 @@
+import bluetooth
+
+host = "190.166.14.96" # The address of Raspberry PI Bluetooth adapter on the server.
+port = 1
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect((host, port))
+while 1:
+    text = input("Enter your message: ") # Note change to the old (Python 2) raw_input
+    if text == "quit":
+        break
+    sock.send(text)
+
+    data = sock.recv(1024)
+    print("from server: ", data)
+
+sock.close()
